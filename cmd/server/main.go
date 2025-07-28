1ab0272e2c77db01a1021b5f47068114f79fd10c
Mihklz <mihkl137@gmail.com>
2025-07-28 16:35:47 +0300

    Первый инкремент

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,21 @@
 package main
 
-func main() {}
+import (
+	"github.com/Mihklz/metrixcollector/internal/handler"
+	"github.com/Mihklz/metrixcollector/internal/repository"
+	"log"
+	"net/http"
+)
+
+func main() {
+	storage := repository.NewMemStorage()
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/update/", handler.NewUpdateHandler(storage))
+
+	log.Println("Starting server at :8080")
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatalf("server failed: %v", err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Mihklz/metrixcollector
+
+go 1.23.4
